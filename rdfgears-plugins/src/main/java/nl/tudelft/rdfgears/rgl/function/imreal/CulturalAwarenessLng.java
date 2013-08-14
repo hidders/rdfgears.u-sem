dc8725a14107f5b4b95ac68644a998eef36e605e
claudia hauff <claudia.hauff@gmail.com>
2013-08-14 18:47:18 +0200

    Updated workflow and function for cultural awareness

--- rdfgears-plugins/src/main/java/nl/tudelft/rdfgears/rgl/function/imreal/CulturalAwarenessLng.java
+++ rdfgears-plugins/src/main/java/nl/tudelft/rdfgears/rgl/function/imreal/CulturalAwarenessLng.java
@@ -1,6 +1,7 @@
 package nl.tudelft.rdfgears.rgl.function.imreal;
 
 import java.util.HashMap;
+
 import nl.tudelft.rdfgears.engine.ValueFactory;
 import nl.tudelft.rdfgears.rgl.datamodel.value.RGLValue;
 import nl.tudelft.rdfgears.util.row.ValueRow;
@@ -33,6 +34,12 @@ public class CulturalAwarenessLng extends TwitterLanguageDetector
 		RGLValue rdfValue2 = inputRow.get(INPUT_UUID);
 		if(rdfValue2!=null)
 			uuid = rdfValue2.asLiteral().getValueString();
+		
+		RGLValue rdfValue3 = inputRow.get(INPUT_USEFRIENDSOFUSER);
+		if (!rdfValue3.isLiteral())
+			return ValueFactory.createNull("Cannot handle URI input in "
+					+ getFullName());
+		String useFriends = rdfValue3.asLiteral().getValueString();	
 
 		HashMap<String, Double> languageMap;
 		try 
